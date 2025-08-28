3bad50be18b0a4e886adc8972bfd5454ba6d5859
xqb-dpx <b.daarr@outlook.com>
2025-08-28 06:14:26 +0330

    cleanup

---
+++ Python/Map.py
@@ -0,0 +1,15 @@
+﻿import folium
+
+m = folium.Map(
+    location=[35.72185811581757, 51.33469323443982],
+    zoom_start=15,
+    tiles="cartodb positron",
+)
+
+folium.Marker(
+    location=[35.72185811581757, 51.33469323443982],
+    popup="Tehran",
+    tooltip="Spark Co.",
+).add_to(m)
+
+m.save("map.html")
